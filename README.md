05cf3c5b709799bcb1afbba0586e9157047be33c
Arjun <arjunair86@gmail.com>
2016-10-09 23:41:57 +0530

    Auto-launch chrome

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-D3 Test
+# D3 Test
+
+Repo created to test the features of D3.js (_a data visualization library_)
+
+### Install:
+```
+git clone https://github.com/arjunair86/D3.git
+cd D3
+npm install
+```
+
+### Run: 
+`npm start`
+
--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const port = process.env.PORT || 1050;
 const app = express();
 
+const exec = require('child_process').exec;
+
+exec(`google-chrome http://localhost:${port}`, (error, stdout, stderr) => {
+	if(error){
+		console.log("exec error: ${error}")
+	}
+	console.log(`stdout: ${stdout}`);
+  	console.log(`stderr: ${stderr}`);
+})
+
 
 app.use("/data", express.static(__dirname + "/data"));
 
@@ -10,5 +20,5 @@ app.get('/', function(req, res){
 })
 
 app.listen(port, function(){
-	console.log("Server listening to", port);
+	console.log(`Server listening to ${port}`);
 });
